1cb731f9accccd0d01b8bac0edd2b04d309bfc72
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2023-07-11 10:53:38 -0300

    feat: Adiciona metodo para buscar varios usuarios com paginacao

--- ecogreen-backend/src/main/java/br/com/ecogreen/ecogreenbackend/repositories/UserRepository.java
+++ ecogreen-backend/src/main/java/br/com/ecogreen/ecogreenbackend/repositories/UserRepository.java
@@ -1,7 +1,10 @@
 package br.com.ecogreen.ecogreenbackend.repositories;
 
 import br.com.ecogreen.ecogreenbackend.models.UserModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +13,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<UserModel, Long> {
 
     Optional<UserModel> findByUsername(String username);
+
+    @Query(value = "select * from \"tb-user\"", nativeQuery = true)
+    Page<UserModel> findAllUsers(Pageable pageable);
 }
