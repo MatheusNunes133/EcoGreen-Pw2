abf294138c6056d469d1ea0a79d04a780342ac0c
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-07-11 16:28:03 -0300

    build: alterando pastas e excluindo pasta components duplicada

--- ecogreen/src/App.tsx
+++ ecogreen/src/App.tsx
@@ -1,5 +1,4 @@
 import './App.css'
-import Register from "./views/Register"
 import { Route, Routes, BrowserRouter } from "react-router-dom"
 import Cadastro from './views/cadastro/index'
 
@@ -8,7 +7,6 @@ function App() {
 
   return (
     <div className="App">
-      <Register />
       <BrowserRouter>
         <Routes>
           <Route element={<Cadastro />} path="Cadastro" />
