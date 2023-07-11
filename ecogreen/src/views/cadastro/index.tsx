491cd664bba3cb6dc6690023989a7e30d17a3953
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-07-11 15:11:11 -0300

    feat: criação base e teste de input na tela cadastro

---
+++ ecogreen/src/views/cadastro/index.tsx
@@ -0,0 +1,12 @@
+import Input from "../../components/input/index"
+import style from "../cadastro/index.css"
+
+const Cadastro = () => {
+    return (
+        <div>
+            <Input/>
+        </div>
+    )
+}
+
+export default Cadastro;
