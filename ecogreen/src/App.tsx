75d5fadf0235da92967ec8cef47084d0013a1951
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-07-11 15:13:43 -0300

    build: adicionando react-router-dom para manipulação de rotas

--- ecogreen/src/App.tsx
+++ ecogreen/src/App.tsx
@@ -1,11 +1,17 @@
 import './App.css'
+import { Route, Routes, BrowserRouter } from "react-router-dom"
+import Cadastro from './views/cadastro/index'
 
 function App() {
-  
+
 
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route element={<Cadastro />} path="Cadastro" />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
