4ba33f8c308c0b01c3f93d56714b652dfceb4c90
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-07-11 16:26:02 -0300

    feat: iniciando tela de cadastro

--- ecogreen/src/views/cadastro/index.tsx
+++ ecogreen/src/views/cadastro/index.tsx
@@ -1,10 +1,20 @@
+import Button from "../../components/button/Button"
 import Input from "../../components/input/index"
-import style from "../cadastro/index.css"
+import "../cadastro/index.css"
 
 const Cadastro = () => {
     return (
-        <div>
-            <Input/>
+        <div className="conteudo">
+            <img src="/ecogreen.png" alt="logo ecogreen" />
+            <h1>Cadastrar-se</h1>
+            <div className="input-container">
+                <Input />
+                <Input />
+                <Input />
+                <Input />
+            </div>
+            <Button name={"Cadastre-se"} />
+            <h4>Já possui uma conta? <a href="/login">Entre aqui!</a> </h4>
         </div>
     )
 }
