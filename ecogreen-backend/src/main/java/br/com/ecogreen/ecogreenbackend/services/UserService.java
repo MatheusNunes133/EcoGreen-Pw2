74582c97c3052217b5ab11310d2dfb9b8797b8af
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2023-07-11 10:54:13 -0300

    feat: Adiciona classe de servico para usuarios

--- ecogreen-backend/src/main/java/br/com/ecogreen/ecogreenbackend/services/UserService.java
+++ ecogreen-backend/src/main/java/br/com/ecogreen/ecogreenbackend/services/UserService.java
@@ -5,18 +5,24 @@ import br.com.ecogreen.ecogreenbackend.enums.EnumRoles;
 import br.com.ecogreen.ecogreenbackend.models.UserModel;
 import br.com.ecogreen.ecogreenbackend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.Map;
+import java.util.Optional;
 
 @Service
 public class UserService {
 
     @Autowired
     private UserRepository repository;
+
+    @Autowired
+    private ExtractUsernameTokenJWT extractUsername;
 
     public Map<String, String> register(RegisterDTO registerDTO){
         try {
@@ -32,4 +38,22 @@ public class UserService {
         }
     }
 
+    public Page<UserModel> getAllUsers(PageRequest page){
+        return repository.findAllUsers(page);
+    }
+
+    public Map<String, String> deleteUser(Long id){
+        try{
+            Optional<UserModel> foundUser = repository.findById(id);
+
+            if(!foundUser.isEmpty()){
+                repository.delete(foundUser.get());
+                return Map.of("success", "Usuário deletado com sucesso!");
+            }else{
+                return Map.of("error", "Erro ao deletar usuário!");
+            }
+        }catch (Exception exception){
+            throw new RuntimeException(exception);
+        }
+    }
 }
