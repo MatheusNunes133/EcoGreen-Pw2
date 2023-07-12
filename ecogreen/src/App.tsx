f3a9f08ff5ff2b0339495d2009845789247a5be5
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-07-11 21:31:20 -0300

    feat: atualizando rotas

--- ecogreen/src/App.tsx
+++ ecogreen/src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Route, Routes, BrowserRouter } from "react-router-dom"
 import Cadastro from './views/cadastro/index'
+import Home from './views/home'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route element={<Home />} path='/' />
           <Route element={<Cadastro />} path="Cadastro" />
         </Routes>
       </BrowserRouter>
