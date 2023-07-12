4ff17508b9a8caff0d18f50d7cc1710777138fc2
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2023-07-12 16:07:23 -0300

    feat: Crai repositorio pra banco de dados e servico pra postagens

---
+++ ecogreen-backend/src/main/java/br/com/ecogreen/ecogreenbackend/repositories/PostRepository.java
@@ -0,0 +1,20 @@
+package br.com.ecogreen.ecogreenbackend.repositories;
+
+import br.com.ecogreen.ecogreenbackend.models.PostModel;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PostRepository extends JpaRepository<PostModel, Long> {
+
+    @Query(value = "select post.*\n" +
+            "from \"tb-post\" post, \"tb-user\" usuario\n" +
+            "where usuario.username = :username and usuario.id = post.user_id", nativeQuery = true)
+    Page<PostModel> findPostsByToken(@Param(value = "username") String username, Pageable pageable);
+}
