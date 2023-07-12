4b4bc6c70109847d743f5f3a7c0d82405bebd675
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2023-07-11 22:45:59 -0300

    fix: Atualiza metodos separando apenas para usuarios normais

--- ecogreen-backend/src/main/java/br/com/ecogreen/ecogreenbackend/controllers/UserController.java
+++ ecogreen-backend/src/main/java/br/com/ecogreen/ecogreenbackend/controllers/UserController.java
@@ -1,12 +1,16 @@
 package br.com.ecogreen.ecogreenbackend.controllers;
 
+import br.com.ecogreen.ecogreenbackend.dto.DataUserDTO;
 import br.com.ecogreen.ecogreenbackend.models.UserModel;
 import br.com.ecogreen.ecogreenbackend.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.Map;
 
@@ -17,17 +21,17 @@ public class UserController {
     @Autowired
     private UserService service;
 
-    @GetMapping("/all-users")
-    @Secured("ROLE_ADMIN")
-    public Page<UserModel> getAll(@RequestParam(defaultValue = "0") int page,
-                                  @RequestParam(defaultValue = "10") int size){
-
-        return service.getAllUsers(PageRequest.of(page, size));
+    @PutMapping(value = "/update-user", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Secured("ROLE_USER")
+    public Map<String, String> updateUser(@RequestHeader("authorization") String token,
+                                          @ModelAttribute UserModel newUser,
+                                          @RequestParam(value = "image", required = false) MultipartFile newFile){
+        return service.updateUser(token, newUser, newFile);
     }
 
-    @DeleteMapping("/delete-user/{id}")
-    @Secured("ROLE_ADMIN")
-    public Map<String, String> deleteUser(@PathVariable Long id){
-        return service.deleteUser(id);
+    @GetMapping("/perfil")
+    @Secured({"ROLE_USER", "ROLE_ADMIN"})
+    public DataUserDTO userPerfil(@RequestHeader("authorization") String token){
+        return service.getUserJWT(token);
     }
 }
