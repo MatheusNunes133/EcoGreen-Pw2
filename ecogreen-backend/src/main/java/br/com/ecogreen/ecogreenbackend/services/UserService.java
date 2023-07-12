64320ecbaa7fb2313c054155d9d28f540e878de4
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2023-07-11 22:48:06 -0300

    feat: Cria metodo pra atualizar e buscar usuario logado e deixa somente metodos pra usuarios normais

--- ecogreen-backend/src/main/java/br/com/ecogreen/ecogreenbackend/services/UserService.java
+++ ecogreen-backend/src/main/java/br/com/ecogreen/ecogreenbackend/services/UserService.java
@@ -1,16 +1,16 @@
 package br.com.ecogreen.ecogreenbackend.services;
 
+import br.com.ecogreen.ecogreenbackend.dto.DataUserDTO;
 import br.com.ecogreen.ecogreenbackend.dto.RegisterDTO;
 import br.com.ecogreen.ecogreenbackend.enums.EnumRoles;
 import br.com.ecogreen.ecogreenbackend.models.UserModel;
 import br.com.ecogreen.ecogreenbackend.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
-
+import org.springframework.web.multipart.MultipartFile;
+import java.io.IOException;
+import java.util.Base64;
 import java.util.Collections;
 import java.util.Map;
 import java.util.Optional;
@@ -38,22 +38,45 @@ public class UserService {
         }
     }
 
-    public Page<UserModel> getAllUsers(PageRequest page){
-        return repository.findAllUsers(page);
-    }
+    public Map<String, String> updateUser(String token, UserModel newUser, MultipartFile file){
+        try {
+            Optional<UserModel> foundUser = repository.findByUsername(extractUsername.extractUsernameToken(token));
 
-    public Map<String, String> deleteUser(Long id){
-        try{
-            Optional<UserModel> foundUser = repository.findById(id);
-
-            if(!foundUser.isEmpty()){
-                repository.delete(foundUser.get());
-                return Map.of("success", "Usuário deletado com sucesso!");
+            if (!foundUser.isEmpty()) {
+                foundUser.get().setUsername(newUser.getUsername());
+                foundUser.get().setName(newUser.getName());
+                foundUser.get().setPassword(new BCryptPasswordEncoder().encode(newUser.getPassword()));
+                if (!file.isEmpty()) {
+                    foundUser.get().setPerfil(file.getBytes());
+                } else {
+                    foundUser.get().setPerfil(null);
+                }
+                repository.save(foundUser.get());
+                return Map.of("success", "Usuário atualizado com sucesso!");
             }else{
-                return Map.of("error", "Erro ao deletar usuário!");
+                return Map.of("error","Erro ao atualizar usuário!");
             }
-        }catch (Exception exception){
+
+        }catch (IOException exception){
             throw new RuntimeException(exception);
         }
+    }
+
+    public DataUserDTO getUserJWT(String token){
+
+        UserModel foundUser = repository.findUserByToken(extractUsername.extractUsernameToken(token));
+        return makeDataUserDTO(foundUser);
+    }
+
+    public static DataUserDTO makeDataUserDTO(UserModel userModel){
+        DataUserDTO dto = new DataUserDTO();
+        dto.setId(userModel.getId());
+        dto.setName(userModel.getName());
+        dto.setUsername(userModel.getUsername());
+        if(userModel.getPerfil() != null){
+            String base64Image = Base64.getEncoder().encodeToString(userModel.getPerfil());
+            dto.setImageBase64(base64Image);
+        }
+        return dto;
     }
 }
