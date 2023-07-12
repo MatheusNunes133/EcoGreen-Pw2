36ec5e379374f1eb8b7889cd7cef1f06f1ed3cc9
Matheus Nunes Miguel <matheusnunesmiguel6@gmail.com>
2023-07-11 22:48:45 -0300

    feat: Cria query para buscar usuarios com o token jwt

--- ecogreen-backend/src/main/java/br/com/ecogreen/ecogreenbackend/repositories/UserRepository.java
+++ ecogreen-backend/src/main/java/br/com/ecogreen/ecogreenbackend/repositories/UserRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -16,4 +17,7 @@ public interface UserRepository extends JpaRepository<UserModel, Long> {
 
     @Query(value = "select * from \"tb-user\"", nativeQuery = true)
     Page<UserModel> findAllUsers(Pageable pageable);
+
+    @Query(value = "select * from \"tb-user\" where username = :username", nativeQuery = true)
+    UserModel findUserByToken(@Param(value = "username") String username);
 }
