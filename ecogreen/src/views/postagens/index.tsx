c03cd8ecaeb842ea2abdee541a42654a279dd413
Kauê Ronald <kaueronald21.kr@gmail.com>
2023-07-12 22:32:01 -0300

    feat: adicionando cards posts integrados a API

--- ecogreen/src/views/postagens/index.tsx
+++ ecogreen/src/views/postagens/index.tsx
@@ -1,21 +1,42 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import Navbar from '../../components/navbar';
 import Button from '../../components/button/index';
 import CriarPostagemModal from '../../components/Modal';
 import CardPost from '../../components/cardPost/index'
 import "./post.css"
+import { api } from '../../service/Api';
 
 const Postagens = () => {
+  const [AllPosts, setAllPosts] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const token: string | null = JSON.parse(localStorage.getItem("tokenJWT") || "null");
+      let posts = await api.get("/post/get-posts", {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      setAllPosts(posts.data.content)
+    })()
+  }, [])
+
   return (
     <div>
       <Navbar autenticacao={true} />
       <div className="buttons-postt">
-          <a href="/todaspostagens"><Button textColor='white' height={50} width={250} name="Todas Postagens" /></a>
-          <a href="/postagens"><Button textColor='white' height={50} width={250} name="Minhas Postagens" /></a>
-          <CriarPostagemModal />
+        <a href="/todaspostagens"><Button textColor='white' height={50} width={250} name="Todas Postagens" /></a>
+        <a href="/postagens"><Button textColor='white' height={50} width={250} name="Minhas Postagens" /></a>
+        <CriarPostagemModal />
       </div>
       <br />
-        <CardPost autenticacao={true} width={400} height={250} image='/perfil.svg' text='conteudo da sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssspostagem' userName='User Name' />
+      {
+        AllPosts.map(item => {
+          return (
+            <CardPost width={400} height={250} image={item.user.perfil} text={item.message} userName={item.user.name} autenticacao={false} />
+          )
+        })
+      }
 
     </div>
   );
--- ecogreen/src/views/todasPostagens/index.tsx
+++ ecogreen/src/views/todasPostagens/index.tsx
@@ -1,42 +1,43 @@
-import React from "react";
+import {useEffect, useState} from "react";
 import Navbar from "../../components/navbar";
 import Button from "../../components/button/index";
 import CriarPostagemModal from "../../components/Modal";
 import CardPost from "../../components/cardPost/index";
 import "../postagens/post.css";
+import { api } from "../../service/Api";
 
 const TodasPostagens = () => {
+  const [AllPosts, setAllPosts] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const token: string | null = JSON.parse(localStorage.getItem("tokenJWT") || "null");
+      let posts = await api.get("/post/get-all-posts", {
+        headers: {
+          Authorization: `Bearer ${token}`
+        }
+      })
+      setAllPosts(posts.data.content)
+    })()
+  }, [])
+
   return (
     <div>
       <Navbar autenticacao={true} />
       <div className="buttons-postt">
-        <a href="/todaspostagens">
-          <Button
-            textColor="white"
-            height={50}   
-            width={250}
-            name="Todas Postagens"
-          />
-        </a>
-        <a href="/postagens">
-          <Button
-            textColor="white"
-            height={50}
-            width={250}
-            name="Minhas Postagens"
-          />
-        </a>
+        <a href="/todaspostagens"><Button textColor='white' height={50} width={250} name="Todas Postagens" /></a>
+        <a href="/postagens"><Button textColor='white' height={50} width={250} name="Minhas Postagens" /></a>
         <CriarPostagemModal />
       </div>
       <br />
-      <CardPost
-        autenticacao={false}
-        width={400}
-        height={250}
-        image="/perfil.svg"
-        text="conteudo da sssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssspostagem"
-        userName="User Name"
-      />
+      {
+        AllPosts.map(item => {
+          return (
+            <CardPost width={400} height={250} image={item.user.perfil} text={item.message} userName={item.user.name} autenticacao={false} />
+          )
+        })
+      }
+
     </div>
   );
 };
